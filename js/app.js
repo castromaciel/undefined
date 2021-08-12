e571382add0f6f66f516f40afe5b2f10ffc8e1e5
Maciel Castro <castromaciel00@gmail.com>
2021-08-12 15:02:21 -0300

    Finalización del Hero

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-
+fetch('https://pruebadejuegosmax.free.beeceptor.com/pruebadejuegos')
+  .then(response => response.json())
+  .then(games => 
+    {
+      const allGames = games;
+      console.log(allGames)
+      for (let i = 0; i < 3; i++) {
+        let imagen = document.getElementById(`gameCard${i}`)
+        imagen.src = allGames[i].images
+        imagen.alt = "gaming"
+        let title = document.getElementById(`gameTitle${i}`)
+        title.textContent = allGames[i].name
+        let paragraph = document.getElementById(`gameDescription${i}`)
+        paragraph.textContent = allGames[i].description
+        
+      }
+    })
+      

10fc015b62d3b47f361497e0012e6dc48bad4a82
Maciel Castro <castromaciel00@gmail.com>
2021-08-24 22:36:11 -0300

    Se agregó la página de administración

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
-fetch('https://pruebadejuegosmax.free.beeceptor.com/pruebadejuegos')
+let allGames = [];
+
+let url = 'http://localhost:3000/games';
+fetch(url)
   .then(response => response.json())
   .then(games => 
     {
-      const allGames = games;
-      console.log(allGames)
-      for (let i = 0; i < 3; i++) {
-        let imagen = document.getElementById(`gameCard${i}`)
-        imagen.src = allGames[i].images
-        imagen.alt = "gaming"
-        let title = document.getElementById(`gameTitle${i}`)
-        title.textContent = allGames[i].name
-        let paragraph = document.getElementById(`gameDescription${i}`)
-        paragraph.textContent = allGames[i].description
-        
-      }
+      chargingGames(games)
     })
-      
+
+function chargingGames(gamesList) {
+  allGames.push(...gamesList);
+  let firstGame = true;
+  let createCarousel = document.getElementById('createCarousel')
+  allGames.forEach(game => 
+  {
+    if(game.outstanding === true)
+    {
+      let carouselItem = document.createElement('div')
+      if (firstGame === true) {
+        carouselItem.className = 'd-flex row carousel-item active'
+        firstGame = false;
+      } else {
+        carouselItem.className = 'd-flex row carousel-item'
+      }
+      carouselItem.innerHTML = 
+        `<img src="${game.images}" class="px-lg-3 col-sm-12 col-md-12 col-lg-12 col-xl-7" alt="" id="gameCard0">
+        <div class="my-3 my-xl-0 px-sm-3 px-lg-3 col-sm-12 col-md-12 box col-lg-12 col-xl-5 d-flex flex-column justify-content-between">
+          <h3 class="fs-md-3 text-truncate" id="gameTitle0">${game.name}</h3>
+          <p class="m-0 px-3 fs-4 reviewText" id="gameDescription0">${game.description}</p>
+          <a href="#" target="_blank" type="button" class="me-5 fs-4 btn btn-outline-light align-self-end">Ver más</a>
+        </div>`;
+      createCarousel.appendChild(carouselItem)
+    }
+  });
+}
+console.log(allGames)

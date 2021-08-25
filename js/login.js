70b6be925dafdb559a99f69f7eb91dfd52fc9472
Castro Maciel <83432755+castromaciel@users.noreply.github.com>
2021-08-25 12:43:31 -0300

    Maciel branch (#14)
    * Se juntaron los archivos

--- js/login.js
+++ js/login.js
@@ -16,6 +16,8 @@ const iniciarSesion = (e) => {
         if(usuario.nombre === 'Admin'){
           // Si el usuario logueado es el administrador, redirigir a la pagina de administracion
           window.location.replace('admin.html')
+          let nav = document.getElementById('nav')
+          console.log(nav)
         }else{
           // Si es un usuario comun, redirigir al home
           window.location.replace('index.html')

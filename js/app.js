66491640c8ecd81dfd56808bb108b8d8bfc761a3
AlejandroBusi <83433159+alejandrobusi@users.noreply.github.com>
2021-08-26 17:38:36 -0300

    Detallejuego (#24)
    * Add new: detalle juego

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 let games = [];
 let allGames = [];
+let url = 'http://localhost:3000/games';
 let filter;
-let url = "http://localhost:3000/games";
+
 const admin = [{
   nombre: "Admin",
   contraseña: "admin",
@@ -12,50 +13,44 @@ const admin = [{
 const usuarios = JSON.parse(localStorage.getItem('usuariosKey')) || [admin]
 
 fetch(url)
-  .then((response) => response.json())
-  .then((games) => {
-    chargingGames(games);
-  });
-axios.get('http://localhost:3000/games')
+.then(response => response.json())
+.then(games => 
+  {
+    chargingGames(games)
+  })
+  axios.get('http://localhost:3000/games')
   .then(apiGames => {
     filterFunction(apiGames)
   })
-class Categories {
-  constructor(games) {
+class Categories{
+  constructor(games){
     this.games = games;
   }
-  saveLocalStorage(saveArr) {
-    localStorage.setItem("arrLocal", JSON.stringify(saveArr));
+  saveLocalStorage (saveArr) {
+  localStorage.setItem("arrLocal" ,JSON.stringify(saveArr))
   }
-  saveById() {
-    let newPage = JSON.parse(localStorage.getItem("arrLocal"));
-    newPage.forEach((element) => {
-      if (element.id == event.target.parentNode.parentNode.id) {
-        console.log(element.id);
-        localStorage.setItem("detailCard", JSON.stringify(element));
-      }
-    });
-  }
-  filtered(cat, games) {
+  saveById () {
+    let newPage =JSON.parse(localStorage.getItem("arrLocal"))
+      newPage.forEach(element => {
+        if (element.id == event.target.parentNode.parentNode.id) {
+          console.log(element.id)
+          localStorage.setItem("detailCard", JSON.stringify(element))
+        }
+      });
+    }
+    filtered(cat, games) {
     this.clearCard();
     let index = 0;
     let gamesFil = [];
-    games.forEach((game) => {
-      game.categories.forEach((category) => {
-        if (category == cat) {
-          gamesFil.push(game);
-          this.cardCreate(
-            gamesFil[index].images[0],
-            gamesFil[index].name,
-            gamesFil[index].description,
-            "./html/gamedetail.html",
-            gamesFil[index].price,
-            game.id
-          );
+    gamess.forEach((game) => {
+      game.categories.forEach ((category) => {
+        if (category == cat){
+          gamesFil.push(game)
+          this.cardCreate(gamesFil[index].images[0], gamesFil[index].name, gamesFil[index].description, "./html/gamedetail.html", gamesFil[index].price, game.id )
           index++;
         }
-      });
-    });
+      })
+    })
     this.saveLocalStorage(gamesFil);
   }
   standOut() {
@@ -63,31 +58,17 @@ class Categories {
     let arrStandOut = [];
     games.forEach((standOut) => {
       if (standOut.outstanding) {
-        arrStandOut.push(standOut);
-        this.cardCreate(
-          standOut.images[0],
-          standOut.name,
-          standOut.description,
-          "./html/gamedetail.html",
-          standOut.price,
-          standOut.id
-        );
+        arrStandOut.push(standOut)
+        this.cardCreate(standOut.images[0], standOut.name, standOut.description, "./html/gamedetail.html", standOut.price, standOut.id)
       }
-    });
-    this.saveLocalStorage(arrStandOut);
-  }
-  allGames() {
-    this.clearCard();
-    this.saveLocalStorage(games);
-    games.forEach((game) => {
-      this.cardCreate(
-        game.images[0],
-        game.name,
-        game.description,
-        "./html/gamedetail.html",
-        game.price,
-        game.id
-      );
+    })
+    this.saveLocalStorage (arrStandOut);
+  } 
+  allGames () {
+    this.clearCard()
+    this.saveLocalStorage (games)
+    games.forEach(game => {
+      this.cardCreate(game.images[0], game.name, game.description, "./html/gamedetail.html", game.price, game.id)
     });
   }
   cardCreate(imgS, cardTitle, subTitle, linkButton, price, id) {
@@ -102,7 +83,7 @@ class Categories {
     image.className = "card-img-top";
     card.appendChild(image);
     let cardBody = document.createElement("div");
-    cardBody.className = "card-body";
+    cardBody.className = "card-body d-flex flex-column justify-content-around text-center";
     card.appendChild(cardBody);
     let title = document.createElement("h5");
     title.className = "card-title";
@@ -113,11 +94,9 @@ class Categories {
     subtitle.innerText = subTitle;
     cardBody.appendChild(subtitle);
     let boton = document.createElement("a");
-    boton.className = "btn btn-warning";
-    boton.innerText = `Comprar $:${price}`;
-    boton.addEventListener("click", (event) => {
-      this.saveById();
-    });
+    boton.className = "btn btn-warning align-self-center d-inline";
+    boton.innerText = `Ver más`;
+    boton.addEventListener("click", (event) => {this.saveById()})
     boton.setAttribute("href", linkButton);
     cardBody.appendChild(boton);
   }
@@ -128,10 +107,45 @@ class Categories {
     }
   }
 }
-// let usuarioRegistered =[];
-// usuarioRegistered.push(admin)
+
 // usuarioRegistered.push(localStorage.getItem('usuariosKey'))
 
+function filterFunction(apiGames) {
+  games.push(...apiGames.data);
+  filter = new Categories(games);
+  filter.allGames();
+}
+function chargingGames(gamesList) {
+  allGames.push(...gamesList);
+  let firstGame = true;
+  let createCarousel = document.getElementById("createCarousel");
+  allGames.forEach((game) => {
+    if (game.outstanding === true) {
+      let carouselItem = document.createElement("div");
+      if (firstGame === true) {
+        carouselItem.className = "d-flex row carousel-item active";
+        firstGame = false;
+      } else {
+        carouselItem.className = "d-flex row carousel-item";
+      }
+      carouselItem.innerHTML = `<img src="${game.images}" class="px-lg-3 col-sm-12 col-md-12 col-lg-12 col-xl-7" alt="" id="gameCard0">
+        <div class="my-3 my-xl-0 px-sm-3 px-lg-3 col-sm-12 col-md-12 box col-lg-12 col-xl-5 d-flex flex-column justify-content-between">
+          <h3 class="fs-md-3 text-truncate" id="gameTitle0">${game.name}</h3>
+          <p class="m-0 px-3 fs-4 reviewText" id="gameDescription0">${game.description}</p>
+          <a href="#" target="_blank" type="button" class="me-5 fs-4 btn btn-outline-light align-self-end">Ver más</a>
+        </div>`;
+      createCarousel.appendChild(carouselItem);
+    }
+  });
+}
+function closeAccount(){
+  localStorage.removeItem("usuarioLogueado");
+  window.location.replace('/index.html');
+}
+
+// localStorage.removeItem('usuariosKey')
+let usuarioRegistered =[];
+usuarioRegistered.push(admin)
 
 let usuario =  JSON.parse(localStorage.getItem('usuarioLogueado'))
 let user = document.getElementById('users')
@@ -193,8 +207,4 @@ function chargingGames(gamesList) {
       createCarousel.appendChild(carouselItem);
     }
   });
-}
-function closeAccount() {
-  localStorage.removeItem("usuarioLogueado");
-  window.location.replace("/index.html");
 }
--- js/gamedetail.js
+++ js/gamedetail.js
@@ -4,8 +4,31 @@ let img0 = document.getElementById("img0").src = `${getObject.images[0]}`
 let img1 = document.getElementById("img1").src = `${getObject.images[1]}`
 let img2 = document.getElementById("img2").src = `${getObject.images[2]}`
 let description = document.getElementById("description").textContent = `${getObject.description}`
-// let cat1 = document.getElementById("cat1").innerText = `${getObject.categories[0]}`
-// let cat2 = document.getElementById("cat2").innerText = `${getObject.categories[1]}`
-// let cat3 = document.getElementById("cat3").innerText = `${getObject.categories[2]}`
-// let cat4 = document.getElementById("cat4").innerText = `${getObject.categories[3]}`
-
+function reqMin() {
+  let soMin = document.getElementById("soMin").textContent = `${getObject.requirementsMin.so}`
+  let procMin = document.getElementById("procMin").textContent = `${getObject.requirementsMin.processor}`
+  let memMin = document.getElementById("memMin").textContent = `${getObject.requirementsMin.memory}`
+  let graphicsMin = document.getElementById("graphicsMin").textContent = `${getObject.requirementsMin.graphics}`
+  let dxMin = document.getElementById("dxMin").textContent = `${getObject.requirementsMin.directx}`
+  let storageMin = document.getElementById("storageMin").textContent = `${getObject.requirementsMin.storage}`
+}
+reqMin();
+function reqMax() {
+  let soMax = document.getElementById("soMax").textContent = `${getObject.requirementsMax.so}`
+  let procMax = document.getElementById("procMax").textContent = `${getObject.requirementsMax.processor}`
+  let memMax = document.getElementById("memMax").textContent = `${getObject.requirementsMax.memory}`
+  let graphicsMax = document.getElementById("graphicsMax").textContent = `${getObject.requirementsMax.graphics}`
+  let dxMax = document.getElementById("dxMax").textContent = `${getObject.requirementsMax.directx}`
+  let storageMax = document.getElementById("storageMax").textContent = `${getObject.requirementsMax.storage}`
+}
+reqMax();
+getObject.categories.forEach(category => {
+  createButton(category)
+});
+function createButton(cat) {
+  let button = document.getElementById("containerButton")
+  let boton = document.createElement("a");
+      boton.className = "btn btn-warning align-self-start mx-1";
+      boton.innerText = `${cat}`;
+      button.appendChild(boton);  
+}
